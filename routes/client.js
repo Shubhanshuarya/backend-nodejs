3cd649fe0660c983497937cc0c8f311b9102756c
Shubhanshu Arya <shubhanshuarya2020@gmail.com>
2023-09-29 14:08:32 +0530

    changes

--- routes/client.js
+++ routes/client.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const clientController = require('../controllers/clientController');
+const { verifyToken } = require('./middleware');
 
 // Client CRUD routes
 router.post('/create', clientController.createClient);
 router.get('/', clientController.getAllClients);
 router.get('/:id', clientController.getClientById);
-router.put('/:id', clientController.updateClient);
+router.put('/:id', clientController.updateClient);D
 router.delete('/:id', clientController.deleteClient);
-router.post('/createAgencyClient', clientController.createAgencyClient);
+router.post('/createAgencyClient', verifyToken, clientController.createAgencyClient);
 router.get('/topClient', clientController.topClient);
 
 module.exports = router;
